99dfbacb352dae81dca2d09134e24e9ecb52b217
ROGO0722 <151502519+ROGO0722@users.noreply.github.com>
2023-11-20 14:55:38 -0500

    Add files via upload

---
+++ jrails/src/main/java/jrails/JRouter.java
@@ -0,0 +1,53 @@
+package jrails;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.lang.reflect.*;
+
+
+public class JRouter {
+    private Map<String, Map <String, String>> routes;
+    public JRouter() {
+        this.routes = new HashMap<>();
+    }
+    public void addRoute(String verb, String path, Class clazz, String method) {
+        String controllerMethod = clazz.getSimpleName() + "#" + method;
+        Map<String, String> param = routes.get(verb);
+        if(param == null) {
+            param = new HashMap<>();
+            routes.put(verb, param);
+        }
+        param.put(verb, controllerMethod);
+    }
+
+    // Returns "clazz#method" corresponding to verb+URN
+    // Null if no such route
+    public String getRoute(String verb, String path) {
+        Map<String, String> param = routes.get(verb);
+        if (param != null) {
+            return param.get(path);
+        }
+        return null;
+    }
+    
+    // Call the appropriate controller method and
+    // return the result
+    public Html route(String verb, String path, Map<String, String> params) {
+        String controllerMethod = getRoute(verb, path);
+        if(controllerMethod != null) {
+            String[] division = controllerMethod.split("#");
+            String cName = division[0];
+            String mName = division[1];
+            try {
+                Class<?> c = Class.forName("jrails." + cName);
+                
+               
+            } catch (Exception e) {
+
+            }
+            
+        }
+        throw new UnsupportedOperationException();
+    }
+}
